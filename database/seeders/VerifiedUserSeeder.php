029dc160ba7e501c4e1b0c57fe30e28d0db93682
Rodolph Khoury <rodolph@tomorrow.services>
2026-03-07 15:01:10 +0200

    seeding a verified user.

---
+++ database/seeders/VerifiedUserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class VerifiedUserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        DB::transaction(function () {
+            User::updateOrCreate(
+                ['email' => 'rodolph.khoury@net.usj.edu.lb'],
+                [
+                    'name' => 'Rodolph Khoury',
+                    'password' => Hash::make('rodolph1234'),
+                    'email_verified_at' => Carbon::now(),
+                ]
+            );
+        });
+    }
+}
